f46a0e06a587f70bce9467ac01bfff79d2fddaf4
AndrewRiabets <Inferion5@gmail.com>
2021-10-28 22:25:27 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,66 @@
-import { Component } from "react";
-import Section from './Components/Section/'
+import { useState } from 'react';
+import Section from './Components/Section/';
 import FeedbackOptions from './Components/FeedbackOptions/FeedbackOptions';
-import Statistics from "./Components/Statistics/Statistics";
-import Notification from './Components/Notification/Notification'
-import styles from './App.module.css'
+import Statistics from './Components/Statistics/Statistics';
+import Notification from './Components/Notification/Notification';
+import styles from './App.module.css';
 
-const x = styles;
-console.log(x)
+const options = ['good', 'neutral', 'bad'];
 
-export default class App extends Component {
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0,
-    };
+export default function App() {
+  const [counterGood, setCounterGood] = useState(0);
+  const [counterNeutral, setCounterNeutral] = useState(0);
+  const [counterBad, setCounterBad] = useState(0);
 
-    handleClick = (e) => {
-        const name = e.target.name;
-        this.setState(prevState => ({
-        [name]: prevState[name] + 1,
-        }));
+  const handleClick = e => {
+    const { name } = e.target;
+    // eslint-disable-next-line default-case
+    switch (name) {
+      case 'good':
+        setCounterGood(prevCounterGood => prevCounterGood + 1);
+        break;
+      case 'neutral':
+        setCounterNeutral(prevCounterNeutral => prevCounterNeutral + 1);
+        break;
+      case 'bad':
+        setCounterBad(prevCounterBad => prevCounterBad + 1);
+        break;
     }
-    
-    feedbacksCounter = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-    };
+  };
 
-    positivePercentageCalculator = () => {
-        const amount = this.feedbacksCounter();
-        const { good } = this.state;
-        const percentage = (good * 100) / amount;
-        return Math.round(percentage);
-    };
+  const feedbacksCounter = () => {
+    return counterGood + counterNeutral + counterBad;
+  };
 
-    render() {
-        const keys = Object.keys(this.state);
-        const total = this.feedbacksCounter();
-        const positivePercentage = this.positivePercentageCalculator();
-        return ( 
-            <div className={styles.App}>
-                <Section title='Please, leave feedback' className={styles.title}>
-                    <FeedbackOptions
-                        options={keys}
-                        handleClick={this.handleClick}
-                    />
-                </Section >
+  const positivePercentageCalculator = () => {
+    return counterGood
+      ? Math.ceil((counterGood / feedbacksCounter()) * 100)
+      : 0;
+  };
 
-                {total === 0 ? <Notification message='No feedback given'/> :
-                <Section title='Statistics' className={styles.title2}>
-                    <Statistics
-                        good={this.state.good}
-                        neutral={this.state.neutral}
-                        bad={this.state.bad}
-                        total={total}
-                        positivePercentage={positivePercentage}/>
-                    </Section>
-                }
-            </div>
-        )
-    }
+  //   const keys = Object.keys(this.state);
+  const total = feedbacksCounter();
+  const positivePercentage = positivePercentageCalculator();
+
+  return (
+    <div className={styles.App}>
+      <Section title="Please, leave feedback" className={styles.title}>
+        <FeedbackOptions options={options} handleClick={handleClick} />
+      </Section>
+
+      {total === 0 ? (
+        <Notification message="No feedback given" />
+      ) : (
+        <Section title="Statistics" className={styles.title2}>
+          <Statistics
+            good={counterGood}
+            neutral={counterNeutral}
+            bad={counterBad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        </Section>
+      )}
+    </div>
+  );
 }
